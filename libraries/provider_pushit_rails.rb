1d89ec5c5ad5fe36b13997c3715fc377659cc0a8
P. Barrett Little <barrett@jackrussellsoftware.com>
2013-12-02 09:50:51 -0500

    Added postgres libraries for database flexibility.

--- libraries/provider_pushit_rails.rb
+++ libraries/provider_pushit_rails.rb
@@ -29,7 +29,9 @@ class Chef
       def initialize(new_resource, run_context = nil)
         @new_resource = new_resource
         @run_context = run_context
+
         @run_context.include_recipe 'mysql::ruby'
+        @run_context.include_recipe 'postgresql::ruby'
 
         @framework = 'rails'
         @bundle_binary = ruby.gem_path('bundle')
