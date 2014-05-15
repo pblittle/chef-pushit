fcd4101b9ac82632f3101052e92e6936f043fc98
P. Barrett Little <barrett@barrettlittle.com>
2014-05-15 14:11:57 -0400

    Added override nginx version attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,14 +28,15 @@ node.default[:mysql][:remove_anonymous_users] = true
 node.default[:mysql][:remove_test_database] = true
 node.default[:mysql][:tunable][:lower_case_table_names] = 1
 
+node.override['nginx']['version'] = '1.4.4'
+node.override['nginx']['source']['version'] = '1.4.4'
+
 node.default['nginx']['init_style'] = 'upstart'
 node.default['nginx']['install_method'] = 'source'
 node.default['nginx']['default_site_enabled'] = false
 node.default['nginx']['dir'] = '/opt/pushit/nginx'
 node.default['nginx']['log_dir'] = '/opt/pushit/nginx/log'
 node.default['nginx']['binary'] = '/opt/pushit/nginx/sbin/nginx'
-node.default['nginx']['version'] = '1.4.4'  # DO NOT CHANGE THIS
-node.default['nginx']['source']['version'] = '1.4.4' # DO NOT CHANGE THIS
 node.default['nginx']['source']['modules'] = [
   'nginx::http_gzip_static_module',
   'nginx::http_ssl_module',
