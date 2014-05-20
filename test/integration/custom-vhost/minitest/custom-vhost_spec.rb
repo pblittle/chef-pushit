d40627538f6221325813d49833e29777d69ae104
Tejay Cardon <tcardon@carekinesis.com>
2014-05-20 08:09:46 -0600

    tests now pass

--- test/integration/custom-vhost/minitest/custom-vhost_spec.rb
+++ test/integration/custom-vhost/minitest/custom-vhost_spec.rb
@@ -8,12 +8,12 @@ describe 'pushit_test::custom-vhost' do
   let(:vhost_config_path) { ::File.join('', 'opt', 'pushit', 'nginx', 'sites-available', 'vhost.conf') }
 
   it 'config file exists' do
-    assert File.exists?(vhost_config_path)
+    assert ::File.exists?(vhost_config_path)
   end
-#
-#   it 'has used our custom template' do
-#     file(vhost_config_path).must_include 'TEST PASSES'
-#   end
+
+  it 'has used our custom template' do
+    assert ::File.readlines(vhost_config_path).grep(/TEST PASSES/).length > 0
+  end
 #
 #   it 'shouldnt pass this' do
 #     file('/opt/pushit/nginx/sites-available/vhost.conf').must_include 'TEST NO PASS'
