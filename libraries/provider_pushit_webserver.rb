e1f5aa7d58e7a3138868b9846493c7248ee24b6b
P. Barrett Little <barrett@barrettlittle.com>
2014-07-28 10:57:38 -0400

    Moved vhost and webserver to library.

---
+++ libraries/provider_pushit_webserver.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+#
+# Author:: P. Barrett Little (<barrett@barrettlittle.com>)
+#
+# Copyright 2013, P. Barrett Little
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'provider_pushit_base'
+
+class Chef
+  class Provider
+    class PushitWebserver < Chef::Provider::PushitBase
+
+      use_inline_resources if defined?(use_inline_resources)
+
+      def action_create
+        super
+
+        recipe_eval do
+          run_context.include_recipe 'nginx::default'
+          run_context.include_recipe 'logrotate::default'
+        end
+
+        webserver_config.run_action(:create)
+        logrotate_app_enable
+      end
+
+      private
+
+      def webserver_config
+        template 'nginx.conf' do
+          path "#{new_resource.config_path}/nginx.conf"
+          cookbook new_resource.config_cookbook
+          source new_resource.config_source
+          owner 'root'
+          group 'root'
+          mode '0644'
+          variables(
+            :log_dir => new_resource.log_dir,
+            :pid_file => new_resource.pid_file,
+            :config_path => new_resource.config_path
+          )
+          action :nothing
+        end
+      end
+
+      def logrotate_app_enable
+        logrotate_app 'nginx' do
+          cookbook 'logrotate'
+          path %w{ ::File.join(new_resource.log_dir, '*.log') }
+          frequency 'daily'
+          rotate 180
+          options %w( missingok dateext delaycompress notifempty compress )
+          create '0644 root root'
+        end
+      end
+    end
+  end
+end
---
+++ libraries/resource_pushit_webserver.rb
@@ -0,0 +1,107 @@
+# encoding: utf-8
+#
+# Author:: P. Barrett Little (<barrett@barrettlittle.com>)
+#
+# Copyright 2013, P. Barrett Little
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/resource/lwrp_base'
+
+class Chef
+  class Resource
+    class PushitWebserver < Chef::Resource::LWRPBase
+
+      self.resource_name = 'pushit_webserver'
+
+      default_action :create
+      actions :create
+
+      def name(arg = nil)
+        set_or_return(
+          :name,
+          arg,
+          :kind_of => [String],
+          :default => 'nginx',
+          :name_attribute => true
+        )
+      end
+
+      def config_cookbook(arg = nil)
+        set_or_return(
+          :config_cookbook,
+          arg,
+          :kind_of => [String],
+          :default => 'pushit'
+        )
+      end
+
+      def config_path(arg = nil)
+        set_or_return(
+          :config_path,
+          arg,
+          :kind_of => [String],
+          :default => node['nginx']['dir']
+        )
+      end
+
+      def config_source(arg = nil)
+        set_or_return(
+          :config_source,
+          arg,
+          :kind_of => [String],
+          :default => 'nginx.conf.erb'
+        )
+      end
+
+      def pid_file(arg = nil)
+        set_or_return(
+          :pid_file,
+          arg,
+          :kind_of => [String],
+          :default => node['nginx']['pid']
+        )
+      end
+
+      def log_dir(arg = nil)
+        set_or_return(
+          :log_dir,
+          arg,
+          :kind_of => [String],
+          :default => node['nginx']['log_dir']
+        )
+      end
+
+      def user(arg = nil)
+        set_or_return(
+          :user,
+          arg,
+          :kind_of => [String],
+          :regex => /^[a-z0-9\-_]+$/,
+          :default => node['nginx']['user']
+        )
+      end
+
+      def group(arg = nil)
+        set_or_return(
+          :group,
+          arg,
+          :kind_of => [String],
+          :regex => /^[a-z0-9\-_]+$/,
+          :default => node['nginx']['group']
+        )
+      end
+    end
+  end
+end
