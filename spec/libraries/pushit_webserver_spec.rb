b45413f238be25a34dc32b0ad0c8ec393dded1e0
Tejay Cardon <tcardon@carekinesis.com>
2014-08-07 10:43:52 -0500

    add a test to ensure we're not creating the template just to destroy it

--- spec/libraries/pushit_webserver_spec.rb
+++ spec/libraries/pushit_webserver_spec.rb
@@ -31,6 +31,10 @@ describe "#{Chef::Provider::PushitWebserver}.delete" do
     expect(chef_run).to delete_template('nginx.conf')
   end
 
+  it 'does not create the nginx config' do
+    expect(chef_run).to_not create_template('nginx.conf')
+  end
+
 # I don't think we should delete the user just because the webserver is deleted.
   it 'removes the user' do
     pending 'not implemented until we have the Pushit_Base.action_delete method ready'
