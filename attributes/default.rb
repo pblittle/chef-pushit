ef3a10f775057b5b24dc979b65b5ae640d9e67a0
Tejay Cardon <tcardon@carekinesis.com>
2014-09-05 07:39:48 -0600

    bump openssl to i release

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,7 +34,7 @@ default[:nodejs][:npm] = '1.4.21'
 
 override['nginx']['version'] = '1.4.4'
 override['nginx']['source']['version'] = '1.4.4'
-override['nginx']['openssl_source']['version']  = '1.0.1h'
+override['nginx']['openssl_source']['version']  = '1.0.1i'
 
 default['nginx']['init_style'] = 'upstart'
 default['nginx']['install_method'] = 'source'
